960de54c32eb5357e6d9e48e61d329d9bba3f7ad
Alexandr <117397307+A14x01@users.noreply.github.com>
2022-11-08 17:37:52 +0100

    Update password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -15,6 +15,7 @@ def login():
                     print("Wrong password or name, please try again.")
             break
 
+
 def view():
     with open("password.txt", "r") as f:
         for line in f.readlines():
@@ -24,20 +25,27 @@ def view():
 
 def add():
     with open("password.txt", "r") as f:
-        for line in f.readlines():
-            saved_data = (line.rstrip())
-            user, password = saved_data.split(":")
-            while True:
-                with open("password.txt", "a") as f:
-                    account_name = input("Account name: ")
-                    account_password = input("Account password: ")
-                    if account_name == user:
-                        print("This account already exist")
-                        pass
-                    else:
-                        f.write(account_name + ":" + account_password)
-                        break
-            break
+        if f.readlines() == []:
+            with open("password.txt", "a") as f:
+                account_name = input("Account name: ")
+                account_password = input("Account password: ")
+                f.write(account_name + ":" + account_password)
+                print("Your first account has been created ")
+        else:
+            for line in f.readlines():
+                saved_data = (line.rstrip())
+                user, password = saved_data.split(":")
+                while True:
+                    with open("password.txt", "a") as f:
+                        account_name = input("Account name: ")
+                        account_password = input("Account password: ")
+                        if account_name == user:
+                            print("This account already exist")
+                            pass
+                        else:
+                            f.write(account_name + ":" + account_password)
+                            break
+                break
 
 while True:
     print("If you want to sign in your account write login.")
